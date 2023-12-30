af6630cc684063740c174f191002cdbcd9c9a7e5
Yogiii13 <133989294+Yogiii13@users.noreply.github.com>
2023-12-30 12:54:23 +0000

    aaj ka kaam pura hua

---
+++ add employee.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $("#addEmployeeButton").click(function() {
+        var employeeId = $("#employeeId").val();
+        var employeeName = $("#employeeName").val();
+        var employeeMiddleName = $("#employeeMiddleName").val();
+        var employeeLastName = $("#employeeLastName").val();
+        var employeeDepartment = $("#employeeDepartment").val();
+        var employeePosition = $("#employeePosition").val();
+        var employeeSalary = $("#employeeSalary").val();
+
+        if (employeeId!= '' && employeeName != '' && employeeMiddleName!= '' && employeeLastName!= '' && employeeDepartment != ''&& employeePosition != '' && employeeSalary != '' )
+         {
+            $("#employeeTable").append("<tr><td>" + employeeId + "</td><td>" + employeeName + "</td><td>" + employeeMiddleName +"</td><td>" +employeeLastName + "</td><td>" +employeeDepartment +"</td><td>" +employeePosition + "</td><td>" +employeeSalary+"</td></tr>");
+            $("#employeeName").val('');
+            $("#employeeMiddleName").val('');
+            $("#employeeLastName").val('');
+            $("#employeeDepartment").val('');
+            $("#employeePosition").val('');
+            $("#employeeSalary").val('');
+        } else {
+            alert('Please enter all the details');
+        }
+    });
+});
